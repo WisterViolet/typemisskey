57e62d034889557149f9079ffe67fd6ab2538ff4
Wister <fujianifan@gmail.com>
2019-07-21 15:34:34 +0900

    .jsonを読み込む関数

--- util/util.go
+++ util/util.go
@@ -1,2 +1,24 @@
 // Package util : 仮に他のSNSで使うときになっても使用できる関数郡
 package util
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// LoadJSON : .jsonを読み込んで、変数に保存する関数
+func LoadJSON(filename string, v interface{}) error {
+	/*
+		filename: .jsonファイルのパス
+		v: 保存先の変数のアドレス
+	*/
+	text, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(text, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
